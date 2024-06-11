c73ee91b2cdce6839ad29da8bf166ef56ad59197
FrancescoVenanti <francesco.venanti@gmail.com>
2024-06-11 09:19:15 +0200

    more small tweaks - still need improvements

--- client/src/Components/LandingPage/LandingPage.tsx
+++ client/src/Components/LandingPage/LandingPage.tsx
@@ -2,13 +2,13 @@ import { Col, Row } from "react-bootstrap";
 import "./LandingPage.scss";
 import LandingImage from "../../assets/Images/RecipeBookLandin.png";
 
-/* TODO add custom button component, Images, Text */
+/* TODO add custom button component, tweak main Image,add seconday image, Text */
 
 const LadingPage = () => {
 	return (
 		<div className="fullScreen">
 			<Row className="p-5 h-100 w-100">
-				<Col xs={12} lg={6} className=" d-flex justify-content-center">
+				<Col xs={12} lg={6} className=" d-flex justify-content-center align-items-center">
 					<img src={LandingImage} alt="landing" className="rounded-5 landingImage" />
 				</Col>
 				<Col xs={12} lg={6} className=" text-center p-2 rounded-5">
