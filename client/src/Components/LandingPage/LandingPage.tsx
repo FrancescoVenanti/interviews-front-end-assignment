ae27afb5565068b17b8f5683d3f399301d7fb4ea
FrancescoVenanti <francesco.venanti@gmail.com>
2024-06-11 09:02:18 +0200

    small tweaks on the image -need improvements

--- client/src/Components/LandingPage/LandingPage.tsx
+++ client/src/Components/LandingPage/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { Col, Row } from "react-bootstrap";
+import "./LandingPage.scss";
 import LandingImage from "../../assets/Images/RecipeBookLandin.png";
 
 /* TODO add custom button component, Images, Text */
@@ -6,9 +7,9 @@ import LandingImage from "../../assets/Images/RecipeBookLandin.png";
 const LadingPage = () => {
 	return (
 		<div className="fullScreen">
-			<Row className="p-5 h-100 w-100 g-4">
-				<Col xs={12} lg={6}>
-					<img src={LandingImage} alt="landing" className="w-100 h-100 rounded-5" />
+			<Row className="p-5 h-100 w-100">
+				<Col xs={12} lg={6} className=" d-flex justify-content-center">
+					<img src={LandingImage} alt="landing" className="rounded-5 landingImage" />
 				</Col>
 				<Col xs={12} lg={6} className=" text-center p-2 rounded-5">
 					<div className="d-flex flex-column justify-content-center align-items-center h-100">
