14b1ef3f111359a3b165c7a8942101d44106eb00
FrancescoVenanti <francesco.venanti@gmail.com>
2024-06-13 12:21:44 +0200

    first approach to add recipe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./Components/HomePage/HomePage";
 import LadingPage from "./Components/LandingPage/LandingPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeDetails from "./Components/Recipe/RecipeDetails";
+import AddRecipe from "./Components/Recipe/AddRecipe";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<Route path="/" Component={LadingPage} />
 				<Route path="/home" Component={HomePage} />
 				<Route path="/recipe/:id" Component={RecipeDetails} />
+				<Route path="add-recipe" Component={AddRecipe} />
 			</Routes>
 		</Router>
 	);
